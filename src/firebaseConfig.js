66788a2447fa09a563d2e86e5de3a99309d05d57
Drasolth01 <egeeren.d@hotmail.com>
2024-06-21 17:06:08 +0300

    Added database

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database"; // Import getDatabase
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCPs5yCvcBwsWFgmBb2sJAMAkKxvjikiDE",
   authDomain: "react-chat-5de7f.firebaseapp.com",
@@ -20,5 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app); // Initialize database
 
-export { database };
+export { database }; // Export database
