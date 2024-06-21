fb854b65b199bda0042a1499802dbb63deb4036f
Drasolth01 <egeeren.d@hotmail.com>
2024-06-21 12:48:25 +0300

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="logo.svg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You clicked the button <code>{count}</code> times.
         </p>
+        <button onClick={incrementCount} className="App-button">
+          Click me
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
